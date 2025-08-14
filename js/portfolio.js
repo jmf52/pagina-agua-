edf7b00985a129649becb7a82425a0f1b374ef8c
Martínez Julio <23302108@utfv.edu.mx>
2025-08-14 07:54:04 -0600

    Create portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    effect: 'fade',
+    loop: true,
+    speed: 300,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+});
+
+
